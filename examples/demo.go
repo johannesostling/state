51766fd783d74b7e4c6c324a6aa952d1beb18b59
Johannes Östling <johannes.ostling@gmail.com>
2018-11-14 10:00:28 +0100

    static parts complete

--- examples/demo.go
+++ examples/demo.go
@@ -1,10 +1,81 @@
 package main
-import (
-	"github.com/johannesostling/state"
-	"fmt"
-)
-func main() {
-	fmt.Println("..")	
 
-	s := state.NewService()
+import (
+	"fmt"
+	"github.com/johannesostling/state"
+)
+
+func main() {	
+	service := state.NewService()
+
+	stateReady := &state.State{Name: "Ready"}
+	stateRiding := &state.State{Name: "Riding"}
+	stateBatteryLow := &state.State{Name: "Battery-low"}
+	stateBounty := &state.State{Name: "Bounty"}
+	stateCollected := &state.State{Name: "Collected"}
+	stateDropped := &state.State{Name: "Dropped"}
+	stateUnknown := &state.State{Name: "Unknown"}
+
+	service.AddState(stateReady)
+	service.AddState(stateRiding)
+	service.AddState(stateBatteryLow)
+	service.AddState(stateBounty)
+	service.AddState(stateCollected)
+	service.AddState(stateDropped)
+	service.AddState(stateUnknown)
+
+	transReadyRidingUser :=
+		state.Transition{stateReady, stateRiding, state.User}
+	transReadyRidingHunter :=
+		state.Transition{stateReady, stateRiding, state.Hunter}
+	service.AddTransition(transReadyRidingUser)
+	service.AddTransition(transReadyRidingHunter)
+
+	transRidingReadyUser :=
+		state.Transition{stateRiding, stateReady, state.User}
+	transRidingReadyHunter :=
+		state.Transition{stateRiding, stateReady, state.Hunter}
+	service.AddTransition(transRidingReadyUser)
+	service.AddTransition(transRidingReadyHunter)
+
+	transRidingBatteryLowInternal :=
+		state.Transition{stateRiding, stateBatteryLow, state.Internal}
+	service.AddTransition(transRidingBatteryLowInternal)
+
+	transBatteryLowBountyInternal :=
+		state.Transition{stateBatteryLow, stateBounty, state.Internal}
+	service.AddTransition(transBatteryLowBountyInternal)
+
+	transBountyCollectedHunter :=
+		state.Transition{stateBounty, stateCollected, state.Hunter}
+	service.AddTransition(transBountyCollectedHunter)
+
+	transCollectedDroppedHunter :=
+		state.Transition{stateCollected, stateDropped, state.Hunter}
+	service.AddTransition(transCollectedDroppedHunter)
+
+	transDroppedReadyHunter :=
+		state.Transition{stateDropped, stateReady, state.Hunter}
+	service.AddTransition(transDroppedReadyHunter)
+
+	transReadyBountyInternal :=
+		state.Transition{stateReady,stateBounty,state.Internal}
+	service.AddTransition(transReadyBountyInternal)
+
+	transReadyUnknownInternal :=
+		state.Transition{stateReady,stateUnknown,state.Internal}
+	service.AddTransition(transReadyUnknownInternal)
+
+
+
+
+	myExample1 :=
+		state.Transition{stateBounty,stateCollected,state.User}
+	err := service.IsValid(myExample1)
+	fmt.Println(err)
+
+	myExample2 :=
+		state.Transition{stateBounty,stateCollected,state.Hunter}
+	err = service.IsValid(myExample2)
+	fmt.Println(err)
 }
--- state.go
+++ state.go
@@ -1,44 +1,61 @@
 package state
+import (
+	"errors"
+	"fmt"
+)
 
 type roles int
 
+//NOTE: role Internal added to make explicit regular service generated
+//		transitions.
 const (
-	user roles = iota
-	hunters
-	admins
+	User roles = iota
+	Hunter
+	Admin
+	Internal
 )
 
 type Service struct {
 	States 				[]*State
-	Transitions 		map[Transition]*Transition
+	Transitions 		map[Transition]bool
 }
 
 func NewService() *Service {
-	s := &Service{
-		
-	}	
+	t := make(map[Transition]bool)
+	s := &Service{Transitions: t}
+	return s
 }
 
-func (s *Service) IsValid(t *Transition) (string,error) {
+func (service *Service) IsValid(t Transition) error {
+	//super users bypass this logic
+	if t.Role == Admin { return nil }
 
+	//known allowed transition
+	if _,ok := service.Transitions[t]; ok {	return nil }
+	
+	//TODO: enum value of role not descriptive enough
+	//unknown transition
+	return errors.New(fmt.Sprintf(
+		"Transition from state \"%s\" to \"%s\" using role %v failed",
+		t.CurrState.Name,t.ReqState.Name, t.Role))	
 }
 
 type State struct {
 	Name  string
 }
 
-func (s *Service) AddState() {
-
+func (service *Service) AddState(state *State) {
+	service.States=append(service.States,state)
 }
 
 type Transition struct {
 	CurrState	*State
 	ReqState	*State
-	User 		roles
+	Role 		roles
 }
 
-func (s *Service) AddTransition() {
-
+func (service *Service) AddTransition(t Transition) {
+	service.Transitions[t] = true
 }
 
 
